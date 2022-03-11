de6c18e7e780f2950c867ba2b413583514e526e9
Diego <alejopov@gmail.com>
2022-03-11 10:58:14 -0500

    Move mongo url to an env variable

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connect() {
-  await mongoose.connect(
-    'mongodb+srv://root:root@cluster0.cynss.mongodb.net/hotel-chain?retryWrites=true&w=majority'
-  );
+  await mongoose.connect(`${process.env.MONGO_URL}`);
 }
 
 export async function disconnect() {
